30f05e0c3d842e0c21820d01fb792ef9464e5d7f
Belfagor2005 <72314956+Belfagor2005@users.noreply.github.com>
2024-11-15 20:18:29 +0100

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 ## setup command=wget -q "--no-check-certificate" https://raw.githubusercontent.com/Belfagor2005/HasBahCa/main/installer.sh -O - | /bin/sh
-exec > >(tee -a /tmp/HasBahCa_debug.txt) 2>&1
-set -x
+
 ## Only This 2 lines to edit with new version ######
 version='1.7'
 changelog='\nAdd Locale Language\nFix Upgrade'
